8dd1ef1702469c7e073a0797981d9c5bda19a6b0
gupta-ashutosh <ashutoshgupta2605@gmail.com>
2016-09-25 18:01:54 +0530

    submitting project for setting and cleaning data

---
+++ README.md
@@ -0,0 +1,21 @@
+Getting and Cleaning Data project
+25-09-2016
+
+====================================
+Files Contained:
+1. codebook.rmd
+2. data.zip
+3. run_analysis.r
+4. tidydata.txt
+5. UCI HAR Dataset
+====================================
+Procedure to run
+1. clone the repo in your local repo
+2. open the file run_analysis.r
+3. this file will use the dataset contained in UCI HAR Dataset at same level
+4. Do not move the dataset or run_analysis file
+5. Open it in R studio and execute the script
+6. After executing the script, a tidydata.txt fill will be generated at same level as run_analysis.r file.
+====================================
+Output 
+tidydata.txt
---
+++ run_analysis.r
@@ -0,0 +1,62 @@
+require(knitr)
+require(markdown)
+library(plyr);
+packages <- c("data.table", "reshape2")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dest <- "."
+download.file(fileUrl,destfile=paste(dest, "data.zip", sep="/"),method="curl")
+unzip(zipfile=paste(dest, "data.zip", sep="/"),exdir=dest)
+
+#reading data from the files
+#setting the directory path
+path_rf <- file.path(dest , "UCI HAR Dataset")
+
+#reading the activity
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+
+#reading the subject
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+
+#reading the features
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
+#1. merging data
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+
+#2. extracting mean and statdard deviation
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+str(Data)
+
+
+#3. Uses descriptive activity names to name the activities in the data set
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+head(Data$activity,30)
+
+#4. Appropriately labels the data set with descriptive variable names.
+names(Data)<-gsub("^tm", "time", names(Data))
+names(Data)<-gsub("^freq", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = paste(dest,"tidydata.txt", sep="/"),row.name=FALSE)
